a8ccdef99be66e9991aca64996fedcfd6810ee32
lucas falcao <lfalcaolopes@gmail.com>
2025-05-23 18:34:13 -0300

    feat(backend): add DTOs for Chore creation, reading, and updating

---
+++ backend/src/Application/DTOs/Chore/ChoreCreateDto.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace Application.DTOs.Chore;
+
+public class ChoreCreateDto
+{
+  [JsonPropertyName("titulo")]
+  public string Title { get; set; } = String.Empty;
+
+  [JsonPropertyName("descricao")]
+  public string? Description { get; set; }
+
+  [JsonPropertyName("dataDeConclusao")]
+  public DateTime? CompletedAt { get; set; }
+
+  [JsonPropertyName("status")]
+  public string Status { get; set; } = String.Empty;
+}
---
+++ backend/src/Application/DTOs/Chore/ChoreReadDto.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace Application.DTOs.Chore;
+
+public class ChoreReadDto
+{
+  [JsonPropertyName("id")]
+  public int Id { get; set; }
+
+  [JsonPropertyName("titulo")]
+  public string Title { get; set; } = String.Empty;
+
+  [JsonPropertyName("descricao")]
+  public string? Description { get; set; }
+
+  [JsonPropertyName("dataDeCriacao")]
+  public DateTime CreatedAt { get; set; }
+
+  [JsonPropertyName("dataDeConclusao")]
+  public DateTime? CompletedAt { get; set; }
+
+  [JsonPropertyName("status")]
+  public string Status { get; set; } = String.Empty;
+}
