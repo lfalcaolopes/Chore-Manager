38d9023310e2147d517895070ffbf8aa4ae309b0
lucas falcao <lfalcaolopes@gmail.com>
2025-05-24 11:36:58 -0300

    feat(backend): add migration to update Status property for string conversion

--- backend/src/Infrastructure/Migrations/20250524011156_Chores-UpdateStatus.Designer.cs
+++ backend/src/Infrastructure/Migrations/20250524011156_Chores-UpdateStatus.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Databases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250524011156_Chores-UpdateStatus")]
+    partial class ChoresUpdateStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
