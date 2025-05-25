067fd14a072fa66d7a0015a10050bcf8575b4343
lucas falcao <lfalcaolopes@gmail.com>
2025-05-25 02:37:59 -0300

    fix(backend): update ChoreUpdateDto and validator to ensure proper status handling and validation

--- backend/src/Application/DTOs/Chores/ChoreUpdateDto.cs
+++ backend/src/Application/DTOs/Chores/ChoreUpdateDto.cs
@@ -6,10 +6,12 @@ namespace Application.DTOs.Chores;
 public class ChoreUpdateDto
 {
   [JsonPropertyName("titulo")]
-  public string? Title { get; set; } = String.Empty;
+  public string Title { get; set; } = String.Empty;
 
   [JsonPropertyName("descricao")]
   public string? Description { get; set; }
+  [JsonPropertyName("dataDeConclusao")]
+  public DateTime? CompletedAt { get; set; }
 
   [JsonPropertyName("status")]
   public ChoreStatus? Status { get; set; }
--- backend/src/Application/Validators/Chores/ChoreUpdateDtoValidator.cs
+++ backend/src/Application/Validators/Chores/ChoreUpdateDtoValidator.cs
@@ -2,7 +2,6 @@ namespace Application.Validators.Chores;
 
 using Application.DTOs.Chores;
 using Domain.Enums;
-using Domain.Utils;
 using FluentValidation;
 
 public class ChoreUpdateDtoValidator : AbstractValidator<ChoreUpdateDto>
@@ -17,5 +16,12 @@ public class ChoreUpdateDtoValidator : AbstractValidator<ChoreUpdateDto>
         .MaximumLength(500).WithMessage("A descrição deve ter no máximo 500 caracteres.")
         .When(x => !string.IsNullOrWhiteSpace(x.Description));
 
+    RuleFor(x => x.Status)
+      .NotNull().WithMessage("O campo 'status' é obrigatório.")
+      .IsInEnum().WithMessage("O status deve ser um valor válido.");
+
+    RuleFor(x => new { x.Status, x.CompletedAt })
+      .Must(x => x.Status == ChoreStatus.Completed || x.CompletedAt == null)
+      .WithMessage("O campo 'dataDeConclusao' só deve ser preenchido quando o status for 'Concluído'.");
   }
 }
